fb95f9d91989ae2ec83d22055c6da1dcfc17bc3b
Roberto Minghi <robertominghi@msn.com>
2017-05-15 21:02:48 +0100

    75% project

---
+++ Gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('styles', function() {
+    gulp.src('sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('styleFiles', function() {
+	return gulp.src([
+		'node_modules/bootstrap/dist/css/bootstrap.min.css'
+	])
+	.pipe(concat('bundle.css'))
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'node_modules/jquery/dist/jquery.min.js',
+		'node_modules/bootstrap/dist/js/bootstrap.min.js',
+		'node_modules/angular/angular.min.js'
+	])
+	.pipe(concat('bundle.js'))
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('iconFiles', function () {
+	return gulp.src([
+		'node_modules/bootstrap/fonts/*'
+	])
+	.pipe(gulp.dest('dist/fonts/'));
+});
+
+gulp.task('build', ['styleFiles', 'scripts', 'styles', 'iconFiles']);
+
+//Watch task
+gulp.task('default', ['styleFiles', 'scripts', 'styles', 'iconFiles'], function() {
+    gulp.watch('sass/*.scss', ['styles']);
+});
---
+++ js/controllers/main.controller.js
@@ -0,0 +1,75 @@
+angular
+    .module('addressbook')
+    .controller('MainController', MainController);
+
+function MainController ($scope) {
+
+	// Controller initialization
+	var vm = this,
+		countId = parseInt(localStorage.getItem("countId")) || 0, // #todo mettere il localStorage se presente
+		fn = {
+			resetForm: function (form) {
+				// Clean the object
+				vm.form = {
+					'id': countId,
+					'name': "",
+					'surname': "",
+					'email': "",
+					'location': ""
+				};
+
+				// Prepare next object
+				countId += 1;
+
+				// Save localstorage idcount
+				localStorage.setItem("countId", countId);
+			}
+		};
+	vm.mailPtr = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
+
+	// Initialize form
+	fn.resetForm();
+
+	// Initialize addressBook
+	vm.addressbook = JSON.parse(localStorage.getItem("addressbook")) || null;
+
+	// Bindable functions
+	vm.saveData = saveData;
+	vm.editContact = editContact;
+	vm.deleteContact = deleteContact;
+
+	// Function to save the data in the form
+	function saveData (form) {
+		if (form.$valid) {
+			if (!vm.addressbook)
+				vm.addressbook = {};
+
+			vm.addressbook[vm.form.id] = vm.form;
+			fn.resetForm(form);
+
+			// Save localstorage addressbook
+			localStorage.setItem("addressbook", JSON.stringify(vm.addressbook));
+		} else {
+			console.log("Check and display error");
+		}
+		
+	};
+
+	// Function to edit a saved contact
+	function editContact (ind) {
+		vm.form = vm.addressbook[ind];
+
+		// Save localstorage addressbook
+		localStorage.setItem("addressbook", JSON.stringify(vm.addressbook));
+	};
+
+	// Function to delete a saved contact
+	function deleteContact (ind) {
+		delete vm.addressbook[ind];
+		if (angular.equals({}, vm.addressbook))
+			vm.addressbook = null;
+
+		// Save localstorage addressbook
+		localStorage.setItem("addressbook", JSON.stringify(vm.addressbook));
+	};
+}
